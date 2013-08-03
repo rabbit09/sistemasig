d50acd710671109f06d6c60f4807604e2f20330c
Dennis Ruiz <darvein@gmail.com>
2013-08-03 16:59:41 -0400

    Symfony2 framework added

--- app/autoload.php
+++ app/autoload.php
@@ -6,8 +6,11 @@ use Composer\Autoload\ClassLoader;
 /**
  * @var $loader ClassLoader
  */
-$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+// Register modules
+AnnotationRegistry::registerAutoloadNamespace(array('Fightmaster' => __DIR__ . '/../vendor/dao/src'));
 
 return $loader;
