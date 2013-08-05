79ec8dc5137da7f0d9bd6a50ff8b83ea9abc802e
Dennis Ruiz <darvein@gmail.com>
2013-08-04 22:37:55 -0400

    Portal web added

---
+++ src/SistemaSig/PortalWebBundle/Resources/public/js/portalweb.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: darvein
+ * Date: 8/4/13
+ * Time: 21:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+jQuery(function ($) {
+    $("#tabs").tabs();
+});
